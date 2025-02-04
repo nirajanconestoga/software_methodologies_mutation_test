5a32922f53a7fe3466ea3437bdf38e0fa4c7d666
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-02-03 23:30:09 -0500

    feat: add a working folder for the mutation test

---
+++ Mutation_Lab/MutationLibrary/MutationClass.cs
@@ -0,0 +1,31 @@
+﻿//Lab3: Mutation Test
+//Name: Nirajan Kumar Bista
+
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using NMutationLab1;
+
+namespace TriangleTest
+{
+    [TestFixture]
+    public class TestTriangles
+    {
+        [Test]
+        public void TestValidIsoscelesTriangle()
+        {
+            // Arrange
+            int firstSide = 40;
+            int secondSide = 60;
+            int thirdSide = 60;
+
+            string expected = "The triangle is an isosceles triangle.";
+
+            // Act
+            string actual = Triangle.IsoscelesTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ Mutation_Lab/Mutation_Lab/Mutation_Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NMutationLab1;
+
+public static class Triangle
+{
+    public static string IsoscelesTriangle(int firstSide, int secondSide, int thirdSide)
+    {
+        string result;
+        if (firstSide == secondSide || firstSide == thirdSide || secondSide == thirdSide)
+        {
+            result = "The triangle is an isosceles triangle.";
+        }
+        else
+        {
+            result = "The triangle is not an isosceles triangle.";
+        }
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Valid isosceles triangle
+        int firstSide = 70;
+        int secondSide = 70;
+        int thirdSide = 60;
+        string result = Triangle.IsoscelesTriangle(firstSide, secondSide, thirdSide);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
